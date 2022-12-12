4c04ae3729569d061edf5d351d62fcf476e1d4bf
srissirs <sarareis2002@gmail.com>
2022-12-12 15:58:33 +0000

    boad with header

--- print_board.pl
+++ print_board.pl
@@ -4,23 +4,29 @@ print_n(S,N) :-
         D is N-1,
         print_n(S, D).
 
-print_header(C):-
-        write('+'),
+print_header_legend(N, N):-
+        write('\n').
+print_header_legend(N, F):-
+        write(' '), write(N), write(' |'), N1 is N + 1, print_header_legend(N1, F).
+
+print_top_edge(C):- print_n(' - +',C), nl,!.
+
+print_line(C,N):-
+        write(' '),
+        write(N),
+        write(' |'),
+        print_n('   |',C-1), nl,
         print_n(' - +',C), nl,!.
 
-print_line(C):-
-        write('|'),
-        print_n('   |',C), nl,
-        write('+'),
-        print_n(' - +',C), nl,!.
+print_matrix(N,N) :- !.
+print_matrix(I,N) :-
+        print_line(N+1,I),
+        D is I+1,
+        print_matrix(D,N).
 
-print_cols(0,C) :- !.
-print_cols(L,C) :-
-        print_line(C),
-        D is L-1,
-        print_cols(D,C).
-
-print_board(L,C) :-
+print_board(N) :-
         nl,
-        print_header(C),
-        print_cols(L,C),!.
+        write('   |'),
+        print_header_legend(0, N),
+        print_top_edge(N+1),
+        print_matrix(0,N),!.
